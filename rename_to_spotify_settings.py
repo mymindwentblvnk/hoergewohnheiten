37ea1b8157918c9be1a57aa0617be5f42718819c
michael-123 <dale.cooper@posteo.de>
2017-12-31 17:54:08 +0100

    Introduce spotify settings for main.py.

---
+++ rename_to_spotify_settings.py
@@ -0,0 +1,13 @@
+SPOTIFY_USERS = {
+    '<user_name>': {
+        'client_id': '<client_id>',
+        'client_secret': '<client_secret>',
+        'redirect_uri': '<redirect_uri>',
+        'user_name': '<user_name>',
+    },
+    # ...
+}
+
+POSTGRES_ENVIRON_KEY = ''
+
+TARGET_TIMEZONE = ''
