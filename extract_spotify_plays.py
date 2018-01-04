27a67d96dae6e63dc9de46783e076baec7b57d66
michael-123 <dale.cooper@posteo.de>
2018-01-04 13:48:24 +0100

    Move dependencies.

--- extract_spotify_plays.py
+++ extract_spotify_plays.py
@@ -1,4 +1,5 @@
 import argparse
+from dateutil import tz
 from datetime import datetime
 
 from spotipy import Spotify
@@ -9,7 +10,30 @@ from models import Play, Track, Album, Artist, PostgreSQLConnection
 import secret_settings
 import util
 
+import settings
 
+
+def set_timezone_to_datetime(datetime_to_set, timezone):
+    return datetime_to_set.replace(tzinfo=tz.gettz(timezone))
+
+
+def convert_played_at_from_response_to_datetime(played_at):
+    try:
+        return datetime.strptime(played_at, '%Y-%m-%dT%H:%M:%S.%fZ')
+    except:
+        # For the single moment where the played at time hits a full second
+        return datetime.strptime(played_at, '%Y-%m-%dT%H:%M:%SZ')
+
+
+def convert_datetime_from_timezone_to_timezone(datetime_to_convert,
+                                               from_tz_code='UTC',
+                                               to_tz_code=settings.TARGET_TIMEZONE):
+    from_tz = tz.gettz(from_tz_code)
+    to_tz = tz.gettz(to_tz_code)
+
+    datetime_to_convert = datetime_to_convert.replace(tzinfo=from_tz)
+    converted_datetime = datetime_to_convert.astimezone(to_tz)
+    return converted_datetime
 class SpotifyConnection(object):
 
     def __init__(self, user_data):
@@ -78,9 +102,9 @@ class SpotifyConnection(object):
             return self.db.session.query(Track).get(track_id)
 
     def get_play_from_played_at_utc_and_track_id(self, played_at_utc, track_id):
-        played_at_utc = util.convert_played_at_from_response_to_datetime(played_at_utc)
-        played_at_utc = util.set_timezone_to_datetime(played_at_utc, timezone='UTC')
-        played_at_cet = util.convert_datetime_from_timezone_to_timezone(played_at_utc)
+        played_at_utc = convert_played_at_from_response_to_datetime(played_at_utc)
+        played_at_utc = set_timezone_to_datetime(played_at_utc, timezone='UTC')
+        played_at_cet = convert_datetime_from_timezone_to_timezone(played_at_utc)
         # Play
         play = Play()
         play.user_name = self.user_name
@@ -145,7 +169,13 @@ def process_hoergewohnheiten(user_name):
 
 
 if __name__ == '__main__':
-    print(util.LOG_HEADER)
+    print('''
+ _     ___   ____  ___   __    ____  _       ___   _     _      _     ____  _  _____  ____  _
+| |_| / / \ | |_  | |_) / /`_ | |_  \ \    // / \ | |_| | |\ | | |_| | |_  | |  | |  | |_  | |\ |
+|_| | \_\_/ |_|__ |_| \ \_\_/ |_|__  \_\/\/ \_\_/ |_| | |_| \| |_| | |_|__ |_|  |_|  |_|__ |_| \|
+''')
+
+
     print("Script started at {}.".format(datetime.now()))
 
     # Argparse
