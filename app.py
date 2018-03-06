a70527718d197aa7d99b67f1de51f7b5fb3380a8
Michael Eichenseer <mymindwentblvnk@posteo.de>
2018-03-06 17:12:25 +0100

    Optimize app initialization.

--- app.py
+++ app.py
@@ -12,37 +12,40 @@ POSTGRES_ENVIRON_KEY = 'DATABASE_URL'
 app = Flask(__name__)
 
 
-def config_app():
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    if POSTGRES_ENVIRON_KEY in os.environ:
-        app.config['SQLALCHEMY_DATABASE_URI'] = \
-            os.environ[settings.POSTGRES_ENVIRON_KEY]
-    else:
-        import secret_settings
-        app.config['SQLALCHEMY_DATABASE_URI'] = \
-            secret_settings.POSTGRES_CONNECTION_STRING
+# ################ #
+# 1) Configure app #
+# ################ #
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+if POSTGRES_ENVIRON_KEY in os.environ:
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        os.environ[settings.POSTGRES_ENVIRON_KEY]
+else:
+    import secret_settings
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        secret_settings.POSTGRES_CONNECTION_STRING
 
 
-def init_db():
-    db.init_app(app)
+# ############################## #
+# 2) Create db connection in app #
+# ############################## #
+db.init_app(app)
 
 
-def init_api():
-    api = Api(app)
-    api.add_resource(Plays,
-        '/plays/user/<string:user_name>')
-    api.add_resource(Counts,
-        '/counts/per/<string:unit>/user/<string:user_name>',
-        '/counts/per/<string:unit>/user/<string:user_name>/from/<string:from_date>',
-        '/counts/per/<string:unit>/user/<string:user_name>/from/<string:from_date>/to/<string:to_date>')
-    api.add_resource(AudioFeature,
-        '/audiofeatures/per/<string:unit>/user/<string:user_name>',
-        '/audiofeatures/per/<string:unit>/user/<string:user_name>/from/<string:from_date>',
-        '/audiofeatures/per/<string:unit>/user/<string:user_name>/from/<string:from_date>/to/<string:to_date>')
+# ################################## #
+# 3) Configure API endpoints for app #
+# ################################## #
+api = Api(app)
+api.add_resource(Plays,
+    '/plays/user/<string:user_name>')
+api.add_resource(Counts,
+    '/counts/per/<string:unit>/user/<string:user_name>',
+    '/counts/per/<string:unit>/user/<string:user_name>/from/<string:from_date>',
+    '/counts/per/<string:unit>/user/<string:user_name>/from/<string:from_date>/to/<string:to_date>')
+api.add_resource(AudioFeature,
+    '/audiofeatures/per/<string:unit>/user/<string:user_name>',
+    '/audiofeatures/per/<string:unit>/user/<string:user_name>/from/<string:from_date>',
+    '/audiofeatures/per/<string:unit>/user/<string:user_name>/from/<string:from_date>/to/<string:to_date>')
 
 
 if __name__ == '__main__':
-    config_app()
-    init_db()
-    init_api()
     app.run(debug=True)
